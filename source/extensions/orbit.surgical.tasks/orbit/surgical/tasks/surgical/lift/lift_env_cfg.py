4a4d3b3afdeaa8320ef018b24c00fc77e685d73e
Xiang Luo <905531567@qq.com>
2026-06-24 23:19:25 +0000

    Got ppo running

--- source/extensions/orbit.surgical.tasks/orbit/surgical/tasks/surgical/lift/lift_env_cfg.py
+++ source/extensions/orbit.surgical.tasks/orbit/surgical/tasks/surgical/lift/lift_env_cfg.py
@@ -172,31 +172,26 @@ class RewardsCfg:
 
     reaching_object = RewTerm(func=mdp.object_ee_distance, params={"std": 0.1}, weight=1.0)
 
-    # lifting_object = RewTerm(func=mdp.object_is_lifted, params={"minimal_height": 0.02}, weight=30.0) # 15
-    lifting_object = RewTerm(func=mdp.object_is_lifted, params={"minimal_height": 0.02}, weight=15.0)
+    lifting_object = RewTerm(func=mdp.object_is_lifted, params={"minimal_height": 0.02}, weight=30.0) # 15
 
     object_goal_tracking = RewTerm(
         func=mdp.object_goal_distance,
         params={"std": 0.3, "minimal_height": 0.02, "command_name": "object_pose"},
-        # weight=30.0, # 16
-        weight=16.0,
+        weight=30.0, # 16
     )
 
     object_goal_tracking_fine_grained = RewTerm(
         func=mdp.object_goal_distance,
         params={"std": 0.05, "minimal_height": 0.02, "command_name": "object_pose"},
-        # weight=10.0, # 5
-        weight=5.0,
+        weight=10.0, # 5
     )
 
     # action penalty
-    # action_rate = RewTerm(func=mdp.action_rate_l2, weight=-1e-4) #-1e-3
-    action_rate = RewTerm(func=mdp.action_rate_l2, weight=-1e-3) 
+    action_rate = RewTerm(func=mdp.action_rate_l2, weight=-1e-4) #-1e-3
 
     joint_vel = RewTerm(
         func=mdp.joint_vel_l2,
-        # weight=-1e-5, #-1e-4
-        weight=-1e-4,
+        weight=-1e-5, #-1e-4
         params={"asset_cfg": SceneEntityCfg("robot")},
     )
 
@@ -221,13 +216,15 @@ class TerminationsCfg:
 class CurriculumCfg:
     """Curriculum terms for the MDP."""
 
-    action_rate = CurrTerm(
-        func=mdp.modify_reward_weight, params={"term_name": "action_rate", "weight": -1e-1, "num_steps": 10000}
-    )
+    # action_rate = CurrTerm(
+    #     func=mdp.modify_reward_weight, params={"term_name": "action_rate", "weight": -1e-1, "num_steps": 10000}
+    # )
 
-    joint_vel = CurrTerm(
-        func=mdp.modify_reward_weight, params={"term_name": "joint_vel", "weight": -1e-1, "num_steps": 10000}
-    )
+    # joint_vel = CurrTerm(
+    #     func=mdp.modify_reward_weight, params={"term_name": "joint_vel", "weight": -1e-1, "num_steps": 10000}
+    # )
+
+    pass
 
     # pass
 
